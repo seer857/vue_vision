8728666ce0bb316c155baffc276a1a50b30cee49
AIMATECH\luoyawen <gitlab.aimatech.com>
2021-01-29 13:49:25 +0800

    2021/01/29 lyw 备份

--- src/views/Organization.vue
+++ src/views/Organization.vue
@@ -2,15 +2,54 @@
 <template>
       <div id=''>
         <h1> 我是组织架构页</h1>
+        <TreeChart :json="treeData" />
       </div>
 </template>
 
 <script>
+import TreeChart from "vue-tree-chart";
 export default {
-   components:{},
+   components:{TreeChart},
    data() {
       return {
-
+treeData: {
+            name: 'root',
+            image_url: "https://static.refined-x.com/avat.jpg",
+            class: ["rootNode"],
+            children: [
+               {
+               name: 'children1',
+               image_url: "https://static.refined-x.com/avat1.jpg"
+               },
+               {
+               name: 'children2',
+               image_url: "https://static.refined-x.com/avat2.jpg",
+               mate: [{
+                  name: 'mate',
+                  image_url: "https://static.refined-x.com/avat3.jpg"
+               }],
+               children: [
+                  {
+                     name: 'grandchild',
+                     image_url: "https://static.refined-x.com/avat.jpg"
+                  },
+                  {
+                     name: 'grandchild2',
+                     image_url: "https://static.refined-x.com/avat1.jpg"
+                  },
+                  {
+                     name: 'grandchild3',
+                     image_url: "https://static.refined-x.com/avat2.jpg"
+                  }
+               ]
+               },
+               {
+               name: 'children3',
+               image_url: "https://static.refined-x.com/avat.jpg"
+               }
+            ]
+         
+      }
        };
     },
    methods: {
